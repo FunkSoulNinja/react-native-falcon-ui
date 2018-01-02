eefa77b1258f169e092c692df6c426dfd6530fc6
Anthony Falcon <TheAnthonyFalcon@gmail.com>
2018-01-02 01:02:52 -0800

    Add nav prop to header and screen components. Various improvements

--- src/MultiScreen.js
+++ src/MultiScreen.js
@@ -23,15 +23,24 @@ const openConfig = {
     toValue: -200
 };
 
+function extractProp(children, propName, index = 0) {
+    if (Array.isArray(children)) {
+        return children[index].props[propName];
+    }
+    return children.props[propName];
+}
+
 //TODO: add ability to give shake Animation for notification
-//TODO: allow for a header/footer to always be shown
-//TODO: add hidden sub header/footer that is shown when zoomed out (open) || additional data
-//TODO: add ability to take in onScreenPress function as a prop.
+//TODO: make it more robust! refactor code, add propTypes, use flowtypes
 
 class MultiScreen extends Component {
     state = {
+        isMoving: false,
+        lastScreenIndex: 0,
         open: false,
-        currentScreenIndex: 0
+        currentScreenIndex: 0,
+        currentScreenName: extractProp(this.props.children, 'screenName'),
+        headerTitle: extractProp(this.props.children, 'headerTitle')
     };
     componentWillMount() {
         this.animated = new Animated.Value(200);
@@ -40,16 +49,38 @@ class MultiScreen extends Component {
             this.animatedValue = _.clamp(value, 0, 200);
         });
     }
+    componentWillReceiveProps(nextProps) {
+        //TODO: add animation when screen is removed with scrollToPage method
+        // if the removed screen was the active screen
+        if (
+            Array.isArray(nextProps.children) &&
+            Array.isArray(this.props.children) &&
+            this.state.currentScreenIndex > nextProps.children.length - 1
+        ) {
+            this.setState(state => ({ ...state, currentScreenIndex: nextProps.children.length - 1 }));
+        }
+
+        if (Array.isArray(this.props.children) && !Array.isArray(nextProps.children)) {
+            this.setState(state => ({ ...state, currentScreenIndex: 0 }));
+        }
+    }
+    componentDidUpdate(prevProps, prevState) {
+        const { currentScreenIndex, lastScreenIndex, open } = this.state;
+        if (!open && lastScreenIndex !== currentScreenIndex) {
+            this.onScreenEnter();
+        }
+    }
     componentWillUnmount() {
         this.animated.removeAllListeners();
     }
     onPanGestureBegin() {
         this.animated.setOffset(this.animatedValue);
+        this.setState(state => ({ ...state, isMoving: true }));
     }
     onGestureEvent = e => {
         const { nativeEvent } = e;
-        const { translationY } = nativeEvent;
-        this.animated.setValue(translationY);
+        const { translationY: dy, translationX: dx } = nativeEvent;
+        this.animated.setValue(dy);
     };
     onPanGestureEnd = ({ translationY: dy, velocityY: vy }) => {
         if (this.state.open) {
@@ -76,40 +107,65 @@ class MultiScreen extends Component {
             this.onPanGestureEnd(nativeEvent);
         }
     };
-    renderHeader() {
-        return (
-            <View style={styles.header}>
-                <BaseButton onPress={this.state.open ? this.close : this.open}>
-                    <Text style={{ fontSize: 36 }}>{this.state.open ? 'close' : 'open'}</Text>
-                </BaseButton>
-            </View>
-        );
-    }
     onScreenPress(screenIndex) {
-        console.log('pressed: ' + screenIndex);
         this.setState(state => ({ ...state, currentScreenIndex: screenIndex }), this.close);
     }
     onScrollEnd = ({ nativeEvent }) => {
         const screenWidth = nativeEvent.layoutMeasurement.width;
         const offsetX = nativeEvent.contentOffset.x;
-        const pageIndex = Math.floor(offsetX / screenWidth);
+        let pageIndex = Math.floor(offsetX / screenWidth);
+        pageIndex = _.clamp(pageIndex, 0, this.nav.lastIndex);
 
         if (this.state.currentScreenIndex !== pageIndex) {
-            this.setState(state => ({ ...state, currentScreenIndex: pageIndex }));
+            this.setState(state => ({
+                ...state,
+                currentScreenIndex: pageIndex,
+                currentScreenName: extractProp(this.props.children, 'screenName', pageIndex),
+                headerTitle: extractProp(this.props.children, 'headerTitle', pageIndex)
+            }));
         }
     };
+    scrollToScreenWithName = name => {
+        if (!Array.isArray(this.props.children)) return;
+        const screenIndex = _.findIndex(this.props.children, screen => screen.props.screenName === name);
+
+        if (screenIndex === -1) return;
+
+        this.scrollToPage(screenIndex);
+    };
     open = () => {
-        Animated.timing(this.animated, {
-            ...openConfig
-        }).start(() => {
-            this.setState(state => ({ ...state, open: true }));
+        return new Promise(resolve => {
+            Animated.timing(this.animated, {
+                ...openConfig
+            }).start(() => {
+                this.setState(state => ({ ...state, open: true, isMoving: false, lastScreenIndex: this.state.currentScreenIndex }));
+            });
+            resolve();
         });
     };
     close = () => {
-        Animated.timing(this.animated, {
-            ...closeConfig
-        }).start(() => {
-            this.setState(state => ({ ...state, open: false }));
+        return new Promise(resolve => {
+            Animated.timing(this.animated, {
+                ...closeConfig
+            }).start(() => {
+                this.setState(state => ({ ...state, open: false, isMoving: false }));
+            });
+            resolve();
+        });
+    };
+    get nav() {
+        return {
+            open: this.open,
+            close: this.close,
+            headerTitle: this.state.headerTitle,
+            goToIndex: this.scrollToPage,
+            go: this.scrollToScreenWithName,
+            lastIndex: Array.isArray(this.props.children) ? this.props.children.length - 1 : 1
+        };
+    }
+    renderHeader = () => {
+        return React.cloneElement(this.props.header, {
+            nav: this.nav
         });
     };
     renderScreens() {
@@ -125,56 +181,112 @@ class MultiScreen extends Component {
             transform: [{ scale }]
         };
         const inactiveScreenStyle = {
+            backgroundColor: 'grey',
             transform: [{ scale: 0.5 }]
         };
 
+        if (!Array.isArray(this.props.children)) {
+            return (
+                <Animated.View key={0} style={[styles.screen, activeScreenStyle]}>
+                    <BaseButton enabled={this.state.open ? true : false} onPress={() => this.onScreenPress(0)} key={0}>
+                        {this.props.children}
+                    </BaseButton>
+                </Animated.View>
+            );
+        }
+
         return _.map(this.props.children, (screen, index) => {
+            if (!screen) return null;
             const isActive = this.state.currentScreenIndex === index;
 
-            // if (!isActive && !this.props.keepMounted && !this.state.open) {
-            //     console.log('ping');
-            //     return <Animated.View key={index} />;
-            // }
+            if (!isActive && !this.props.keepMounted) {
+                return <Animated.View key={index} style={[styles.screen, inactiveScreenStyle]} />;
+            }
 
             return (
-                <Animated.View
-                    key={index}
-                    style={[styles.screen, isActive ? activeScreenStyle : inactiveScreenStyle]}
-                >
-                    <BaseButton
-                        enabled={this.state.open ? true : false}
-                        onPress={() => this.onScreenPress(index)}
-                        key={index}
-                    >
-                        {screen}
+                <Animated.View key={index} style={[styles.screen, isActive ? activeScreenStyle : inactiveScreenStyle]}>
+                    <BaseButton enabled={this.state.open ? true : false} onPress={() => this.onScreenPress(index)} key={index}>
+                        {React.cloneElement(screen, {
+                            nav: this.nav
+                        })}
                     </BaseButton>
                 </Animated.View>
             );
         });
     }
+    scrollToPage = async index => {
+        const x = SCREEN_WIDTH * index;
+        await this.open();
+        await new Promise(resolve => setTimeout(() => resolve(), 300));
+        this.scrollViewRef.scrollTo({ x, animated: true });
+        await new Promise(resolve => setTimeout(() => resolve(), 300));
+        this.close();
+    };
+    renderUI() {
+        const opacity = this.animated.interpolate({
+            inputRange: [0, 100, 200],
+            outputRange: [1, 0, 0]
+        });
+        if (this.state.isMoving || this.state.open) {
+            const topBarStyle = {
+                flexDirection: 'row',
+                justifyContent: 'flex-start',
+                alignItems: 'flex-end',
+                position: 'absolute',
+                height: '20%',
+                width: '100%',
+                top: 0,
+                opacity
+            };
+            const bottomBarStyle = {
+                // backgroundColor: 'blue',
+                flexDirection: 'row',
+                justifyContent: 'flex-start',
+                alignItems: 'flex-start',
+                position: 'absolute',
+                height: '20%',
+                width: '100%',
+                bottom: 0,
+                opacity
+            };
+            return [
+                <Animated.View key={1} style={topBarStyle}>
+                    {this.props.topBar}
+                </Animated.View>,
+                <Animated.View key={2} style={bottomBarStyle}>
+                    {this.props.bottomBar}
+                </Animated.View>
+            ];
+        }
+    }
+    onScreenEnter() {
+        if (!this.props.onScreenEnter) return;
+        this.props.onScreenEnter(this.state.currentScreenName || this.state.headerTitle || this.state.currentScreenIndex);
+    }
     render() {
         return (
             <View style={styles.container}>
-                {this.renderHeader()}
-                <PanGestureHandler
-                    minPointers={3}
-                    onGestureEvent={this.onGestureEvent}
-                    onHandlerStateChange={this.onHandlerStateChange}
-                >
-                    <ScrollView
-                        // ref={ref => (this.scrollViewRef = ref)}
-                        scrollEnabled={this.state.open}
-                        onMomentumScrollEnd={this.onScrollEnd}
-                        pagingEnabled
-                        horizontal
-                        style={[
-                            styles.screenContainer,
-                            { backgroundColor: this.props.backgroundColor }
-                        ]}
+                <View style={{ width: '100%', height: '100%' }}>
+                    {this.renderHeader()}
+                    <PanGestureHandler
+                        enabled={this.props.enableSwipe}
+                        minPointers={this.props.minPointers}
+                        onGestureEvent={this.onGestureEvent}
+                        onHandlerStateChange={this.onHandlerStateChange}
                     >
-                        {this.renderScreens()}
-                    </ScrollView>
-                </PanGestureHandler>
+                        <ScrollView
+                            ref={ref => (this.scrollViewRef = ref)}
+                            scrollEnabled={this.state.open}
+                            onMomentumScrollEnd={this.onScrollEnd}
+                            pagingEnabled
+                            horizontal
+                            style={[styles.screenContainer, { backgroundColor: this.props.backgroundColor }]}
+                        >
+                            {this.renderScreens()}
+                        </ScrollView>
+                    </PanGestureHandler>
+                    {/* {this.renderUI()} */}
+                </View>
             </View>
         );
     }
@@ -182,7 +294,9 @@ class MultiScreen extends Component {
 
 MultiScreen.defaultProps = {
     backgroundColor: 'rgb(64, 64, 64)',
-    keepMounted: true
+    keepMounted: true,
+    minPointers: 3,
+    enableSwipe: true
 };
 
 const styles = StyleSheet.create({
@@ -197,14 +311,11 @@ const styles = StyleSheet.create({
     screen: {
         width: SCREEN_WIDTH,
         height: '100%'
+        // backgroundColor: 'rgb(144, 144, 144)'
     },
     screenContainer: {
         width: '100%',
         height: '100%'
-    },
-    header: {
-        backgroundColor: 'rgb(43, 143, 139)',
-        width: '100%'
     }
 });
 
